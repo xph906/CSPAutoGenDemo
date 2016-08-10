fb3fbea62a657b94a50247f92b9c3a65e6e49c9e
xph906 <xiangpan2011@u.northwestern.edu>
2016-08-10 15:21:53 -0500

    complete the list of packages to be installed.

--- install_package.sh
+++ install_package.sh
@@ -7,3 +7,9 @@ npm install deep-diff --save
 npm install parse-domain --save
 npm install util --save
 npm install mkdirp --save
+npm install express --save
+npm install body-parser --save
+npm install morgan --save
+npm install cors --save
+npm install formidable --save
+npm install base-64 --save
